33d2ca377705d2ccef42a85d08b8760e656dd450
Miles Ager <miles.ager13@gmail.com>
2023-07-21 15:06:32 -0500

    add splash page header and image

---
+++ app/components/views/Splash.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+
+export default function Splash() {
+  return (
+    <>
+      <h1>Unlock Limitless Collaboration and Productivity at CoHive</h1>
+      <h2>Step into CoHive, your home for innovative software development. We meticulously build bespoke software solutions that drive your business forward in the digital era.</h2>
+      <Image src='/beehiveimage.jpg' width={500} height={500} alt="beehive" />
+    </>
+  );
+  // Contact us button on this view
+};
