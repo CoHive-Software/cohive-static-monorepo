db84767fafa42596efc6956e3f35cd057c8b8963
eekbk <eekeric@gmail.com>
2023-07-26 21:34:43 -0500

    refactor: rename url

--- app/components/views/Splash.tsx
+++ app/components/views/Splash.tsx
@@ -1,17 +1,15 @@
 import Image from "next/image";
 import ContactUsButton from "../buttons/ContactUsButton";
 
-
-
 export default function Splash() {
   return (
     <div>
       <div>
-        <h1>Unlock Limitless Collaboration and Productivity at CoHive</h1>
+        <h1 className="text-orange">Unlock Limitless Collaboration and Productivity at CoHive</h1>
         <h2>Step into CoHive, your home for innovative software development. We meticulously build bespoke software solutions that drive your business forward in the digital era.</h2>
         <ContactUsButton />
       </div>
-      <Image src='/beehiveimage.jpg' width={500} height={500} alt="beehive" />
+      <Image src='/beehiveImage.jpg' width={500} height={500} alt="beehive" />
       </div>
       );
   // Contact us button on this view
