6164536e06a0c0c8ba487bb2aead6c2359408a21
eekbk <eekeric@gmail.com>
2023-07-27 00:22:56 -0500

    style: update height of home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Splash from "./components/views/Splash";
 
 export default function Page() {
   return (
-    <>
+    <div className="h-full">
       <Splash />
-    </>
+    </div>
   );
   //<h2 className={'font-sans'}>Hello, CoHive!</h2>
 
