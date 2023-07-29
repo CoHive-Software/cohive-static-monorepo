48b537ce272c8bbd5eed66de09b14f45821e91b8
dREWbxKewb <drewvasqu1432@gmail.com>
2023-07-29 01:40:48 -0500

    Update: styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,13 @@ export const metadata: Metadata = {
   title: 'CoHive Software',
   description:
     "Welcome to CoHive Software, a leading software engineering company empowering businesses with cutting-edge technology solutions. Our expert team of software engineers delivers exceptional software development, web development, mobile app development, and cloud services. With a focus on innovation and client satisfaction, we provide tailored solutions that optimize your digital presence, enhance efficiency, and drive growth. Contact us today for reliable, scalable, and secure software engineering services that unlock your business's true potential.",
-  };
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
     <html lang='en' className={`${lato.variable} ${abril.variable}`}>
       <body>
