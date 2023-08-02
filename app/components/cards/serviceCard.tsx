65bf3b6b3d52e4622c8e7a7ef81aab083e1f9a19
matthewpmi <mpmekita@gmail.com>
2023-08-02 17:50:00 -0400

    CREATE: new component to render service cards

---
+++ app/components/cards/serviceCard.tsx
@@ -0,0 +1,18 @@
+import ContactUsButton from "../buttons/ContactUsButton";
+
+interface ServiceCardProps {
+  title: string;
+  description: string
+}
+
+export default function ServiceCard({ title, description }: ServiceCardProps) {
+  return (
+    <div className="bg-gradient-to-t from-gray to-gray-mid p-4 rounded-lg flex flex-col justify-center items-center">
+      <h3 className="text-center text-pearl text-lg font-bold mb-2">{title}</h3>
+      <p className="text-center text-gray-light">{description}</p>
+      <div className="mt-4">
+        <ContactUsButton />
+      </div>
+    </div>
+  );
+}
