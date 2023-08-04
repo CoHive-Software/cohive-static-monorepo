5274318d3dfcd60b705cdae56563443c79f51263
Miles Ager <miles.ager13@gmail.com>
2023-08-04 13:32:53 -0500

    STYLE: add width to service card

--- app/components/cards/serviceCard.tsx
+++ app/components/cards/serviceCard.tsx
@@ -7,7 +7,7 @@ interface ServiceCardProps {
 
 export default function ServiceCard({ title, description }: ServiceCardProps) {
   return (
-    <div className="bg-gradient-to-t from-gray to-gray-mid p-4 rounded-lg flex flex-col justify-center items-center">
+    <div className="bg-gradient-to-t from-gray to-gray-mid p-4 rounded-lg flex flex-col justify-center items-center w-72">
       <h3 className="text-center text-pearl text-lg font-bold mb-2">{title}</h3>
       <p className="text-center text-gray-light">{description}</p>
       <div className="mt-4">
