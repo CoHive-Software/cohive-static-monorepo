a3ccb95d08b1199f56fb856d247f0d883dd21535
matthewpmi <mpmekita@gmail.com>
2023-08-07 12:39:33 -0400

    STYLE: add div to wrap contact us h1, add contact id

--- app/components/contact/contact.tsx
+++ app/components/contact/contact.tsx
@@ -28,59 +28,62 @@ export default function Contact() {
   };
 
   return (
-    <div className='flex flex-row justify-between'>
-      <form className='flex flex-col w-full xl:w-7/12 text-pearl' onSubmit={handleSubmit}>
-        <div className='flex flex-col md:flex-row'>
-          <div className='flex flex-col flex-grow'>
-            <span className='m-1'>Name*</span>
+    <div id='contact' className='flex flex-col'>
+      <h1 className='mx-1 mb-3 text-2xl font-light tracking-widest'>Contact Us</h1>
+      <div className='flex flex-row justify-between'>
+        <form className='flex flex-col w-full xl:w-7/12 text-pearl' onSubmit={handleSubmit}>
+          <div className='flex flex-col md:flex-row'>
+            <div className='flex flex-col flex-grow'>
+              <span className='m-1'>Name*</span>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={handleNameChange}
+                required
+                className='border-2 border-black rounded-md px-2 py-1 md:mr-2 bg-gray-light text-black focus:outline-none'
+              />
+            </div>
+            <div className='flex flex-col flex-grow mt-2 md:mt-0'>
+              <span className='m-1'>Business</span>
+              <input
+                type="text"
+                id="business"
+                value={business}
+                onChange={handleBusinessChange}
+                className='border-2 border-black flex-grow rounded-md px-2 py-1 bg-gray-light text-black focus:outline-none'
+              />
+            </div>
+          </div>
+          <div className='flex flex-col mt-2'>
+            <span className='m-1'>Email*</span>
             <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={handleNameChange}
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
               required
-              className='border-2 border-black rounded-md px-2 py-1 md:mr-2 bg-gray-light text-black focus:outline-none'
+              className='border-2 border-black rounded-md px-2 py-1 bg-gray-light text-black focus:outline-none'
             />
           </div>
-          <div className='flex flex-col flex-grow mt-2 md:mt-0'>
-            <span className='m-1'>Business</span>
-            <input
-              type="text"
-              id="business"
-              value={business}
-              onChange={handleBusinessChange}
-              className='border-2 border-black flex-grow rounded-md px-2 py-1 bg-gray-light text-black focus:outline-none'
+          <div className='flex flex-col mt-2'>
+            <span className='m-1'>Message*</span>
+            <textarea
+              id="message"
+              value={message}
+              onChange={handleMessageChange}
+              required
+              rows={6}
+              className='border-2 border-black rounded-md px-2 py-1 bg-gray-light text-black focus:outline-none'
             />
           </div>
+          <div className='mt-3'>
+            <SubmitButton />
+          </div>
+        </form>
+        <div className='xl:flex items-center justify-center hidden'>
+          <Image src='/mail.svg' width={360} height={360} alt={'Mail Image'} />
         </div>
-        <div className='flex flex-col mt-2'>
-          <span className='m-1'>Email*</span>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-            className='border-2 border-black rounded-md px-2 py-1 bg-gray-light text-black focus:outline-none'
-          />
-        </div>
-        <div className='flex flex-col mt-2'>
-          <span className='m-1'>Message*</span>
-          <textarea
-            id="message"
-            value={message}
-            onChange={handleMessageChange}
-            required
-            rows={6}
-            className='border-2 border-black rounded-md px-2 py-1 bg-gray-light text-black focus:outline-none'
-          />
-        </div>
-        <div className='mt-3'>
-          <SubmitButton />
-        </div>
-      </form>
-      <div className='xl:flex items-center justify-center hidden'>
-        <Image src='/mail.svg' width={360} height={360} alt={'Mail Image'} />
       </div>
     </div>
   )
