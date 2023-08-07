36f2bcd57f91e52c22e9afef585cf15446d8acd3
matthewpmi <mpmekita@gmail.com>
2023-08-07 12:37:10 -0400

    REFACTOR: add link to contact component

--- app/components/buttons/ContactUsButton.tsx
+++ app/components/buttons/ContactUsButton.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function ContactUsButton() {
 
   return (
-    <button className="w-fit bg-orange-light hover:bg-yellow py-4 px-12 rounded-xl">Contact Us</button>
+    <Link href='#contact'>
+      <button className="w-fit bg-orange-light hover:bg-yellow py-4 px-12 rounded-xl">Contact Us</button>
+    </Link>
   );
 
 };
