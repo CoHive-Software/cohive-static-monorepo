9dfe6799c697311d69786e0e904419c07bf3c944
colin <colin.williams.dev@gmail.com>
2023-08-11 22:23:39 -0500

    chore: migrate bot in parcel

---
+++ apps/discord-ticket-bot/src/index.js
@@ -0,0 +1,72 @@
+import { discord } from "./discord.js";
+import { sendTest, sendTwilioSMS } from "./twilio.js";
+import members from "./members/index.js";
+import welcome from "./welcome.js";
+
+
+/**
+ *  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * ^^^^^^^^^^ EVENT LISTENERS ^^^^^^^^^^
+ *  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ */
+
+// TODO EVENT HAPPENS
+// MAKE TWILIO API CALL
+// // members.json
+// // --> grab phone numbers, emails
+// // --> twilio sends out notifications (info sent)
+// .then((data) => Discord "NOTIFICATIONS SENT")
+// .error((err) => Discord "I couldn't send it")
+
+/**
+ * @see https://old.discordjs.dev/#/docs/discord.js/main/general/welcome
+ * @description: for BOT API documentation
+ */
+
+
+discord.on("ready", async (bot) => {
+  console.log(`✅ ${bot.options.rest.authPrefix} ${bot.user.tag} is online! Listening to channels: ${bot.channels}`);
+
+  const channel = discord.channels.cache.get("1132439106899693588");
+  const messagesInChannel = await channel.messages.fetch();
+
+  if (messagesInChannel.size === 0) {
+    // channel.send("_", {
+    //   files: ["https://res.cloudinary.com/dbdyc4klu/image/upload/v1690063402/EMAIL_Circle_Logo_Yellow_Font_tp1emb.png"]
+    // });
+    // channel.send(logo);
+    channel.send("https://res.cloudinary.com/dbdyc4klu/image/upload/v1690063402/EMAIL_Circle_Logo_Yellow_Font_tp1emb.png");
+    channel.send(welcome);
+  }
+});
+
+/**
+ * @description use `message.channel` to validate channel from channel data
+ */
+discord.on("messageCreate", (message) => {
+  /* this validation disallows bots from responding to each other/themselves, remove at your own risk 💀 */
+  if (message.author.bot) return;
+
+  console.log("LOOK HERE", message.createdAt + 24);
+
+  if (message.channel.parentId === "1126197975983591534" && message.channel.messageCount === 1) {
+    const projectName = message.channel.name;
+    const projectOwner = message.author.username;
+    const projectCreatedAt = message.createdAt;
+    // sendTwilioSMS(projectName, projectOwner, projectCreatedAt);
+    console.log("WE MADE IT!", projectName, projectOwner, projectCreatedAt);
+    message.reply("( ͡° ͜ʖ ͡°)");
+  }
+
+  /* message sent in server from any user: */
+  console.log(`Discord message: "${message.content}" from User: ${message.author.username} at ${message.createdAt}`);
+  console.log("CHANGE NAME:", message.author, message);
+
+  /* bot will react to any message sent with this emoji */
+  // message.react('🤓');
+});
+
+discord.login(process.env["TOKEN"]);
+
+// WORKS!
+// sendTwilioSMS();
---
+++ apps/discord-ticket-bot/src/members/index.js
@@ -0,0 +1,18 @@
+
+const members = [
+  { name: "Miles Ager", email: "miles.ager13@gmail.com", phone: "+15045648893" },
+  { name: "Colin Williams", email: "colin.williams.dev@gmail.com", phone: "+18454173238" },
+  { name: "Benjamin Lyon", email: "carlbenjaminlyon@gmail.com", phone: "+19859519956" },
+  { name: "Matthew Mekita", email: "mpmekita@gmail.com", phone: "+12016022084" },
+  { name: "Jessica Torres", email: "jessicatorres.debug@gmail.com", phone: "+15046212549" },
+  { name: "Andrew Vasquez", email: "andrewvasquez.dev@gmail.com", phone: "+17609754217" },
+  { name: "Jorge Carvajal", email: "", phone: "" },
+  { name: "Neil Shapiro", email: "neilshapiro.dev@gmail.com", phone: "+15043399511" },
+  { name: "Benjamin Dickinson", email: "bsadickinson@gmail.com", phone: "" },
+  { name: "Sam Littell", email: "samlittellmds@gmail.com", phone: "+15124963213" },
+  // { name: "Kalypso Homan", email: "", phone: "+15042582433" },
+  // { name: "Jordan Mann", email: "jordan.mann.dev@gmail.com", phone: "+15713191640" },
+  // { name: "Marcus Ager", email: "marcus.ager.dev@gmail.com", phone: "+15045648891" }
+];
+
+export default members;
