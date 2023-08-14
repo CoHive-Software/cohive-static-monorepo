1548953417f83bcecd434bb41372950eb890a09a
matthewpmi <mpmekita@gmail.com>
2023-08-14 13:58:49 -0400

    CREATE: projects component

---
+++ app/components/views/Projects.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Projects() {
+  return (
+    <div className='flex flex-col justify-center items-start w-7/12'>
+      <span>Completed Projects</span>
+    </div>
+  );
+}
