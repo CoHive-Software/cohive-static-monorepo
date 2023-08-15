c4e57e7815bf486ce4d764d365bd3e8c750068c4
matthewpmi <mpmekita@gmail.com>
2023-08-15 14:58:11 -0400

    UPDATE: add responsive rendering logic to Projects instead of layout

--- app/components/views/Projects.tsx
+++ app/components/views/Projects.tsx
@@ -25,12 +25,12 @@ export default function Projects() {
   }
 
   return (
-    <div id='projects' className='flex flex-col justify-center items-start w-7/12 mb-4'>
+    <div id='projects' className='hidden md:flex flex-col justify-center items-start w-7/12 mb-4'>
       <hr className='h-0.5 bg-gradient-to-r from-transparent via-white to-transparent w-full mt-3 mb-10' />
       <h2 className='text-white text-3xl font-light tracking-wider mb-6'>Completed <span className='text-orange'>Projects</span></h2>
       <div className='flex flex-col'>
         <Image
-          className='mb-2 rounded-md min-h-['
+          className='mb-2 rounded-md'
           src={currentProject[0]}
           alt={currentProject[2]}
           width={1065}
